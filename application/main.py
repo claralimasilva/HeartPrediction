35e8fd5ebe8ba02bc355473762ad00f54a2442aa
claralimasilva <claralimasilva@gmail.com>
2024-04-19 00:01:37 -0300

    Organizando projeto antigo

---
+++ application/main.py
@@ -0,0 +1,32 @@
+from typing import Union
+from pydantic import BaseModel
+from fastapi import FastAPI
+import pandas as pd
+import pickle
+
+pickle_win = open('model\\model.pkl', 'rb')
+model = pickle.load(pickle_win)
+print(model)
+
+class BodyModel(BaseModel):
+    Age: int
+    Sex: str
+    ChestPainType: str
+    RestingBP: int
+    Cholesterol: int
+    FastingBS: int
+    RestingECG: str
+    MaxHR: int
+    ExerciseAngina: str
+    Oldpeak: float
+    ST_Slope: str
+
+app = FastAPI()
+
+@app.post('/predict')
+async def predict(body: BodyModel):
+    data = body.model_dump()
+    data = pd.DataFrame(data, index=[0])
+    prediction = model.predict(data)
+    return {"prediction": prediction}
+
